e80e8865c1d85f1c9bcdb77554775f1f434ac821
YUK1-0000 <116713322+YUK1-0000@users.noreply.github.com>
2022-11-28 02:35:26 +0000

    ff

--- BlackJack.py
+++ BlackJack.py
@@ -1,7 +1,14 @@
 import random
 
 
-cards_number = [[i+1 for i in range(13)] for _ in range(4)]
+cards_number = [["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"] for _ in range(4)]
 player = []
+show_cards = ""
 
-player.append(cards_number[random.randint(0, 3)])
+
+for _ in range(2):
+    player.append(cards_number[random.randint(0, 3)][random.randint(0,12)])
+
+
+show_cards = (" ".join(player))
+print(show_cards)
