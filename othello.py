cff7c887673a6e00ed1498af37058f391a6bd98a
YUK1 <116713322+YUK1-0000@users.noreply.github.com>
2022-12-01 00:27:25 +0000

    rrr

--- othello.py
+++ othello.py
@@ -1,77 +1,101 @@
 class Board:
-    edge = 8
-    WHITE = 1
-    BLACK = -1
+    EDGE = 3
+    WIN_REACH = 3
+    PIECE = ("", "○", "●")
+    MARU = 1
+    BATSU = -1
     EMPTY = 0
-    ALLOWED_NUMBERS = [str(i+1) for i in range(edge)]
-    AXIS = ("０", "１", "２", "３", "４", "５", "６" ,"７", "８")
-    def __init__(self)  -> None:
-        self.grid_data = [[0 for _ in range(8)] for _ in range(8)]
-        self.grid_data[3][3] = self.grid_data[4][4] = self.WHITE
-        self.grid_data[3][4] = self.grid_data[4][3] = self.BLACK
-        self.turn = -1
-        
-    def set_(self)  -> None:
-        "駒を置く。"
-        self.turn_pieces = 0
+    ALLOWED_NUMBERS = [str(i+1) for i in range(EDGE)]
+    AXIS = [i for i in range(10)]
 
+
+    def __init__(self):
+        self.grid_data = [[0 for _ in range(self.EDGE)] for _ in range(self.EDGE)]
+        self.piece = -1
+
+
+    def input_(self):
         while True:
-            X = input("X = ")
-            Y = input("Y = ")
-            if X in self.ALLOWED_NUMBERS and Y in self.ALLOWED_NUMBERS:
-                X, Y = int(X)-1, int(Y)-1
-                if self.grid_data[Y][X] != self.EMPTY:
-                    print("既に駒があります。")
-                    continue
-                for i in range(-1, 2):
-                    for j in range(-1, 2):
-                        if 0 <= Y+i <= self.edge-1 and 0 <= X+j <= self.edge-1:
-                            if self.grid_data[Y+i][X+j] == self.turn*-1:
-                                for n in range(self.edge):
-                                    if 0 <= Y+i*(n+1) <= self.edge-1 and 0 <= X+j *(n+1) <= self.edge-1:
-                                        if self.grid_data[Y+i*(n+1)][X+j*(n+1)] == self.turn*-1:
-                                            self.turn_pieces += 1
-                                        elif self.grid_data[Y+i*(n+1)][X+j*(n+1)] == self.turn and self.turn_pieces > 0:
-                                            for m in range(self.turn_pieces):
-                                                self.grid_data[Y+i*m][X+j*m] = self.turn
-                                            break
-                break
-            else:
-                print("1~8で入力してください。")
-        
-    def show(self)  -> None:
-        "ボードを表示します。"
-        self.grid = [[0 for _ in range(8)] for _ in range(8)]
-        for i in range(self.edge+1):
-            print(self.AXIS[i], end = "")
-        print()
-        for i in range(self.edge):
-            print(self.AXIS[i+1], end = "")
-            for j in range(self.edge):
-                if self.grid_data[j][i] == self.BLACK:
-                    print("黒", end = "")
-                elif self.grid_data[j][i] == self.WHITE:
-                    print("白", end = "")
+            self.show()
+            print("\nYou :", self.PIECE[self.piece], "\n1 ~", self.EDGE, "で入力してください。")
+            x = input("X = ")
+            y = input("Y = ")
+            if x in self.ALLOWED_NUMBERS and y in self.ALLOWED_NUMBERS:
+                x, y = int(x)-1, int(y)-1
+                if self.grid_data[y][x] != self.EMPTY:
+                    print("そのマスは空いていません。")
                 else:
-                    print("＋", end = "")      
+                    return x, y
+            else:
+                print("1 ~", self.EDGE, "で入力してください。")
+
+
+    def set_(self, x, y):
+        print(x, y)
+        self.grid_data[y][x] = self.piece
+
+
+    def show(self):
+        self.grid = [[0 for _ in range(self.EDGE)] for _ in range(self.EDGE)]
+        for i in range(self.EDGE+1):
+            print(self.AXIS[i], end = " ")
+        print()
+        for i in range(self.EDGE):
+            print(self.AXIS[i+1], end = " ")
+            for j in range(self.EDGE):
+                if self.grid_data[i][j] == self.BATSU:
+                    print("x", end = " ")
+                elif self.grid_data[i][j] == self.MARU:
+                    print("o", end = " ")
+                else:
+                    print(" ", end = " ")      
             print()
 
-
-def trial(self)  -> bool:
-        "ゲーム終了かどうかを返す。"
-
-class WindowBoard(Board):
-    def computer(self)  -> None:
-        "コンピューターにやらせます。"
-
-    def show(self)  -> None:
-        "ウィンドウでボードを表示します。"
+    
+    def grid_check(self, i, j):
+        if self.grid_data[i][j] == self.piece:
+            return True
 
 
+    def arround_check(self, i, j, n, m):
+        if (n != 0 or m != 0) and (0 <= i+n <= self.EDGE-1 and 0 <= j+m <= self.EDGE-1):
+            if self.grid_data[i+n][j+m] == self.piece:
+                return True
+
+
+    def count(self, i, j, n, m):
+        count = 1
+        for t in range(self.EDGE):
+            if 0 <= i+n*(t+1) <= self.EDGE-1 and 0 <= j+m*(t+1) <= self.EDGE-1:
+                if self.grid_data[i+n*(t+1)][j+m*(t+1)] == self.piece:
+                    count += 1
+                    if count == self.WIN_REACH:
+                        self.show()
+                        print("\n" + self.PIECE[self.piece], "WIN")
+                        return True
+                    elif turn == self.EDGE**2:
+                        self.show()
+                        print("\nDRAW")
+                        return True
+
+
+    def trial(self):
+        for i in range(self.EDGE):
+            for j in range(self.EDGE):
+                if self.grid_check(i, j):
+                    for n in (-1, 0, 1):
+                        for m in (-1, 0, 1):
+                            if self.arround_check(i, j, n, m):
+                                if self.count(i, j, n, m):
+                                    return True
 
 
 board = Board()
-board.turn  *=  -1
+turn = 0
 while True:
-    board.show()
-    board.set_()
+    board.piece *= -1
+    turn += 1
+    x, y = board.input_()
+    board.set_(x, y)
+    if board.trial():
+        break
