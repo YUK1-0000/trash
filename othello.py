fc82bd18329a839202099eea095f947da7fe5044
YUK1-0000 <tanaka543taro@gmail.com>
2022-12-04 14:06:02 +0900

    AA

--- othello.py
+++ othello.py
@@ -1,21 +1,79 @@
 class Board:
     EDGE = 8
+    WIN_REACH = 3
     PIECE = (" ", "○", "●")
     WHITE = 1
     BLACK = -1
     EMPTY = 0
     ALLOWED_NUMBERS = [str(i+1) for i in range(EDGE)]
-    AXIS = [i for i in range(9)]
+    AXIS = [i for i in range(10)]
 
 
     def __init__(self):
         self.grid_data = [[0 for _ in range(self.EDGE)] for _ in range(self.EDGE)]
-        self.grid_data[3][3] = self.grid_data[4][4] = self.WHITE
-        self.grid_data[3][4] = self.grid_data[4][3] = self.BLACK
         self.piece = -1
 
+            
+    def show(self):
+        print()
+        self.grid = [[0 for _ in range(self.EDGE)] for _ in range(self.EDGE)]
+        for i in range(self.EDGE+1):
+            print(self.AXIS[i], end = " ")
+        print()
+        for i in range(self.EDGE):
+            print(self.AXIS[i+1], end = " ")
+            for j in range(self.EDGE):
+                print(self.PIECE[self.grid_data[i][j]], end = " ")
+            print()
+            
 
-    def input(self):
+    def set_(self, x, y):
+        self.grid_data[y][x] = self.piece
+
+
+
+    
+    def grid_check(self, i, j):
+        if self.grid_data[i][j] == self.piece:
+            return True
+
+
+    def around_check(self, i, j, n, m):
+        if (n != 0 or m != 0) and (0 <= i+n <= self.EDGE-1 and 0 <= j+m <= self.EDGE-1):
+            if self.grid_data[i+n][j+m] == self.piece:
+                return True
+
+
+    def result(self, i, j, n, m):
+        count = 1
+        for t in range(self.EDGE):
+            if 0 <= i+n*(t+1) <= self.EDGE-1 and 0 <= j+m*(t+1) <= self.EDGE-1:
+                if self.grid_data[i+n*(t+1)][j+m*(t+1)] == self.piece:
+                    count += 1
+                    if count == self.WIN_REACH:
+                        self.show()
+                        print("\n" + self.PIECE[self.piece], "WIN")
+                        return True
+        if turn == self.EDGE**2:
+            self.show()
+            print("\nDRAW")
+            return True
+
+
+    def trial(self):
+        for i in range(self.EDGE):
+            for j in range(self.EDGE):
+                if self.grid_check(i, j):
+                    for n in (-1, 0, 1):
+                        for m in (-1, 0, 1):
+                            if self.around_check(i, j, n, m):
+                                if self.result(i, j, n, m):
+                                    return True
+
+
+class TUIBoard(Board):
+
+    def input_(self):
         re = False
         emp = True
 
@@ -51,82 +109,17 @@ class Board:
                 continue
             else:
                 return x, y
-            
 
 
-    def set(self, x, y):
-        self.grid_data[y][x] = self.piece
-
-
-    def show(self):
-        print()
-        self.grid = [[0 for _ in range(self.EDGE)] for _ in range(self.EDGE)]
-        for i in range(self.EDGE+1):
-            print(self.AXIS[i], end = " ")
-        print()
-        for i in range(self.EDGE):
-            print(self.AXIS[i+1], end = " ")
-            for j in range(self.EDGE):
-                print(self.PIECE[self.grid_data[i][j]], end = " ")
-            print()
-
-######
-    def around_check(self, x, y, i, j):
-        if 0 <= y+i <= self.EDGE-1 and 0 <= x+j <= self.EDGE-1:
-            if self.grid_data[y+i][x+j] == self.piece*-1:
-                return True
-
-
-    def count(self, x, y, i, j, count):
-        for n in range(self.EDGE):
-            if self.grid_data[y+i*(n+1)][x+j*(n+1)] == self.piece*-1:
-                count += 1
-            elif self.grid_data[y+i*(n+1)][x+j*(n+1)] != self.piece*-1:
-                return 
-
-
-
-    
-    def count(self, i, j, n, m):
-        count = 1
-        for t in range(self.EDGE):
-            if 0 <= i+n*(t+1) <= self.EDGE-1 and 0 <= j+m*(t+1) <= self.EDGE-1:
-                if self.grid_data[i+n*(t+1)][j+m*(t+1)] == self.piece:
-                    count += 1
-                else:
-
-                    
-######
-    
-
-    def turn(self, x, y):
-        for i in (-1, 0, 1):
-            for j in (-1, 0, 1):
-
-                if 0 <= y+i <= self.EDGE-1 and 0 <= x+j <= self.EDGE-1:
-                    if self.grid_data[y+i][x+j] == self.piece*-1:
-
-                        count = 1
-                        for n in range(self.EDGE):
-                            if self.grid_data[y+i*(n+1)][x+j*(n+1)] == self.piece*-1:
-                                count += 1
-                            elif self.grid_data[y+i*(n+1)][x+j*(n+1)] == self.piece:
-                                for m in range(count):
-                                    self.set(x, y)
-
-                        
-                        
-
-
-                    
-
-board = Board()
+        
+board = TUIBoard()
 turn = 0
 while True:
     board.piece *= -1
     turn += 1
 
-    x, y = board.input()
-    board.set(x, y)
-    if board.turn(x, y):
+    x, y = board.input_()
+    board.set_(x, y)
+    if board.trial():
         break
+
