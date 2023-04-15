f950a19111d8469ba17c782103c0207b985e414c
YUK1 <116713322+YUK1-0000@users.noreply.github.com>
2023-04-15 01:49:13 +0000

    oo

--- Prime_factorization.py
+++ Prime_factorization.py
@@ -1,13 +1,11 @@
 n = int(input())
-ls = []
 
 def a(x):
     if x <= 1:
         return
     for i in range(2, x + 1):
         if x % i == 0:
-            ls.append(i)
+            print(i)
             return a(int(x / i))
 
 a(n)
-print(ls)
