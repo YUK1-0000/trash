b18865bdf2dcccd9893fed80aba0d82e4cde3e99
YUK1 <116713322+YUK1-0000@users.noreply.github.com>
2023-06-24 16:06:02 +0000

    new: じゃんけん.rs

--- じゃんけん/src/main.rs
+++ じゃんけん/src/main.rs
@@ -8,6 +8,7 @@ fn main(){
     loop{
         let _cpu_hand = (rand::thread_rng().gen::<f32>() * 2.).round();
         let mut my_hand = String::new();
+        
         stdin().read_line(&mut my_hand).unwrap();
         println!("{}", my_hand);
         break;
