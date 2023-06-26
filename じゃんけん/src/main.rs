1c4d5b0b375a63f38454cab93ad0f5bb54e7098b
YUK1 <116713322+YUK1-0000@users.noreply.github.com>
2023-06-26 21:57:20 +0000

    ja

--- じゃんけん/src/main.rs
+++ じゃんけん/src/main.rs
@@ -1,16 +1,17 @@
 use std::io::{stdin, stdout};
 use rand::Rng;
 
-const _HANDS: [&str;3] = ["グー", "チョキ", "パー"];
+const HANDS: [&str;3] = ["グー", "チョキ", "パー"];
 
 
 fn main(){
     loop{
-        let _cpu_hand = (rand::thread_rng().gen::<f32>() * 2.).round();
-        let mut my_hand = String::new();
-        
-        stdin().read_line(&mut my_hand).unwrap();
-        println!("{}", my_hand);
+        let mut i = String::new();
+        stdin().read_line(&mut i).unwrap();
+        let j = (rand::thread_rng().gen::<f32>() * 2.).round() as u8;
+        let my_hand = HANDS[0];
+        let cpu_hand = HANDS[0];
+        println!("You: {}\nCPU: {}\ni: {}\nj: {}", my_hand, cpu_hand, i, j);
         break;
     };
 }
